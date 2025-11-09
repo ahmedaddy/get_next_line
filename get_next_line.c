6ac396b28417e84fa1706a5924ac6242f22ef2b1
= <=>
2025-11-09 19:47:27 +0100

    initialize project

---
+++ get_next_line.c
@@ -0,0 +1,25 @@
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    static char *str;
+    
+}
+int main(void)
+{
+    int fd;
+    char *line;
+
+    fd = open("test.txt", O_RDONLY);
+    if (fd == -1)
+        return (1);
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    close(fd);
+    return (0);
+}
